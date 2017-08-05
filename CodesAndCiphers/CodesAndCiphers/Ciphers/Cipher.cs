d9582d19fae47d11876ab2f39439f68549ef9015
smithbinah <bsmith@student.neumont.edu>
2017-08-04 20:48:06 -0600

    Initial
    first upload

---
+++ CodesAndCiphers/CodesAndCiphers/Ciphers/Cipher.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodesAndCiphers.Ciphers
+{
+    public class Cipher : INotifyPropertyChanged
+    {
+        private string answer;
+        public int RowIndex { get; set; }
+        public int IndexMax { get; set; }
+
+        public string Answer
+        {
+            get { return answer; }
+            set
+            {
+                answer = value;
+                OnPropertyChanged("Answer");
+            }
+        }
+        private string encryptedInput;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string EncryptedInput
+        {
+            get { return encryptedInput; }
+            set
+            {
+                encryptedInput = value;
+                reverseInput();
+                groupInput();
+                OnPropertyChanged("EncryptedInput");
+            }
+        }
+
+        private void groupInput()
+        {
+           
+            string placeHolder = "";
+            foreach (char item in EncryptedInput.ToCharArray())
+            {
+                if (RowIndex >= IndexMax)
+                {
+                    placeHolder += "\n";
+                    RowIndex++;
+                    RowIndex = 0;
+                }
+                else
+                {
+                    placeHolder += "\t"+item;
+                    RowIndex++;
+                }
+
+            }
+            Groups = placeHolder;
+        }
+
+        private string groups;
+
+        public string Groups
+        {
+            get { return groups; }
+            set
+            {
+                groups = value;
+                OnPropertyChanged("Groups");
+            }
+        }
+
+
+        public void reverseInput()
+        {
+
+            char[] chars = EncryptedInput.ToCharArray();
+            Array.Reverse(chars);
+            string str = new string(chars);
+            Answer = str.Replace(" ", String.Empty);
+
+
+
+        }
+
+
+        private void OnPropertyChanged(string info)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+
+
+
+
+    }
+}
---
+++ CodesAndCiphers/CodesAndCiphers/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CodesAndCiphers.Ciphers;
+
+namespace CodesAndCiphers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Cipher ciphers;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ciphers = new Cipher();
+            setContext();
+        }
+
+        public void setContext()
+        {
+            InputText.DataContext = ciphers;
+            AnswerBox.DataContext = ciphers;
+            Groupings.DataContext = ciphers;
+        }
+
+        private void InputText_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            ciphers.EncryptedInput = InputText.Text;
+        }
+
+        private void IncreaseRowButton_Click(object sender, RoutedEventArgs e)
+        {
+            ciphers.IndexMax++;
+        }
+
+        private void DecreaseRowButton_Click(object sender, RoutedEventArgs e)
+        {
+            ciphers.IndexMax--;
+        }
+    }
+}
